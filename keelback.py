9517bcd2d0837134ca4ff1f10d43648bc654db9c
Rutherford Craze <acs@craze.co.uk>
2021-01-29 16:59:53 +0100

    Add some progress

--- keelback.py
+++ keelback.py
@@ -1,5 +1,5 @@
 import os, pathlib
-import markdown
+import markdown, pystache
 
 DIR_CONTENT     = "Content"
 DIR_TEMPLATES   = "Templates"
@@ -11,32 +11,65 @@ class Page:
     def __init__(
         self,
         path,
-        slug,
-        content = None
+        title
         ):
-        self.slug = slug
         self.path = path
-        self.content = content
+        self.title = title
+        self.slug = title.lower().replace(" ", "_")
 
     def __repr__(self):
         return "Page: " + self.slug
 
+    @property
+    def content(self):
+        with open(os.path.join(self.path, self.title + ".txt"), 'r') as f:
+            content = f.read().strip()
+        return content
+
+    @property
     def body(self):
-        if self.content:
-            return markdown.markdown(self.content)
-        return None
+        return markdown.markdown(self.content)
+
+    @property
+    def crumb(self):
+        path = self.path.split("/")
+        
+        if self.slug == "index":
+            crumb = []
+        else:
+            crumb = [get_link(inventory["index"])]
+            for point in path[1:]:
+                if point in inventory:
+                    crumb.append(get_link(inventory[point]))
+        crumb.append(get_link(self, True))
+
+        return " / ".join(crumb)
+    
+
+    @property
+    def render_props(self):
+        return dict(
+            vars(self),
+            body = self.body,
+            crumb = self.crumb)
+    
 
 class Category:
     def __init__(
         self,
-        slug,
+        title,
         pages = []
         ):
-        self.slug = slug
+        self.title = title
         self.pages = pages
+        self.slug = title.lower().replace(" ", "_")
 
     def __repr__(self):
         return "Category: " + self.slug
+
+    @property
+    def render_props(self):
+        return dict(vars(self))
 
 ###
 
@@ -47,10 +80,34 @@ def get_inventory():
         current_dir = path.split('/')[-1]
 
         if current_dir != "Media":
+            inventory[current_dir] = Category(current_dir)
             for file in files:
                 if file.endswith(".txt"):
-                    if current_dir not in inventory:
-                        inventory[current_dir] = []
-                    inventory[current_dir].append(Page(path, file[:-4]))
+                    title = file[:-4]
+                    inventory[title] = Page(path, title)
+                    inventory[current_dir].pages.append(inventory[title])
 
     return inventory
+
+def get_link(page, highlit = False):
+    template = "<a href='#{slug}'>{title}</a>"
+    if highlit:
+        template = "<a href='#{slug}' class='highlit'>{title}</a>"
+    return template.format(slug=page.slug, title=page.title)
+
+def render_page(slug):
+    if slug not in inventory:
+        return "404"
+    else:
+        page = inventory[slug]
+
+    with open(os.path.join(DIR_TEMPLATES, "page.html"), 'r') as f:
+        template = f.read()
+
+    r = pystache.Renderer()
+    return r.render(template, page.render_props)
+
+inventory = get_inventory()
+# print(inventory)
+# print(inventory["Recent Post"].body)
+print(render_page("Recent Post"))
